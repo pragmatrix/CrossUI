b9bed595c691b209f4200af824750b23848faecf
Armin Sander <armin@replicator.org>
2012-07-08 15:45:01 +0200

    add a separate test case to visualize the test in the readme

---
+++ CrossUI.Tests/ReadMeTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CrossUI.Tests
+{
+	namespace MyDrawings
+	{
+		public class RoundedRectangleTest
+		{
+			[BitmapDrawingTest(Width = 80, Height = 40)]
+			public void roundedRect(IDrawingContext context)
+			{
+				context.roundedRect(0, 0, 80, 40, 8);
+			}
+		}
+	}
+}
